e036957a7f8879f0d57d0a4bc95483e9e99e132e
Shiv Prakash Anand <shivakp2111@gmail.com>
2018-03-28 21:19:32 +0530

    28-03-2018

---
+++ ecommerce/forms.py
@@ -0,0 +1,116 @@
+
+from django import forms
+
+# all the Django forms must inherit forms.Form
+class ContactForm(forms.Form):
+    
+    # using fullname, email, content in request.POST.get()
+    fullname = forms.CharField (
+                    widget=forms.TextInput(
+                        attrs = {
+                            "class": "form-control",
+                            "placeholder": "You Full Name",
+                        }
+                    )
+                )
+    email =   forms.EmailField (
+                    widget=forms.TextInput(
+                        attrs = {
+                            "class": "form-control",
+                            "placeholder": "Your Email",
+                        }
+                    )
+                )
+    content = forms.CharField (
+                    widget=forms.Textarea(
+                        attrs = {
+                            "class": "form-control",
+                            "placeholder": "Your Content",
+                        }
+                    )
+                )
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if not "gmail.com" in email:
+            raise forms.ValidationError("Email has to be gmail.com")
+        return email
+
+class LoginForm(forms.Form):
+
+    username = forms.CharField (
+                    widget=forms.TextInput (
+                        attrs= {
+                            "class": "form-control",
+                            "placeholder": "give username"
+                        }
+                    )
+                )
+    password = forms.CharField (
+                    widget=forms.PasswordInput (
+                        attrs= {
+                            "class": "form-control",
+                            "placeholder": "give password"
+                        }
+                    )
+                )
+
+class RegisterForm(forms.Form):
+
+    firstname = forms.CharField (
+                    widget=forms.TextInput (
+                        attrs= {
+                            "class": "form-control",
+                            "placeholder": "your firstname"
+                        }
+                    )
+                )
+    lastname =  forms.CharField (
+                    widget=forms.TextInput (
+                        attrs= {
+                            "class": "form-control",
+                            "placeholder": "your lastname"
+                        }
+                    )
+                )
+    username =  forms.CharField (
+                    widget=forms.TextInput (
+                        attrs= {
+                            "class": "form-control",
+                            "placeholder": "your username"
+                        }
+                    )
+                )
+    password =  forms.CharField (
+                    widget=forms.PasswordInput (
+                        attrs= {
+                            "class": "form-control",
+                            "placeholder": "your password"
+                        }
+                    )
+                )
+    password_confirm =  forms.CharField (
+                    label= 'Confirm Password',
+                    widget=forms.PasswordInput (
+                        attrs= {
+                            "class": "form-control",
+                            "placeholder": "your password"
+                        }
+                    )
+                )
+    email =     forms.EmailField (
+                    widget=forms.TextInput (
+                        attrs= {
+                            "class": "form-control",
+                            "placeholder": "your email"
+                        }
+                    )
+                )
+    phone_number =     forms.IntegerField (
+                    widget=forms.NumberInput (
+                        attrs= {
+                            "class": "form-control",
+                            "placeholder": "phone number"
+                        }
+                    )
+                )
---
+++ ecommerce/views.py
@@ -0,0 +1,83 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from .forms import ContactForm
+from .forms import LoginForm
+from .forms import RegisterForm
+
+def home_page(request):
+    # add different context here
+    context = {
+        "title": "This is from context of home page",
+        "content": "Home Page"
+    }
+    if request.user.is_authenticated():
+        name = request.user.username
+        context["premium_content"] = "Yeahh.. Hello "+name
+    return render(request, "home_page.html", context)
+
+
+def about_page(request):
+    context = {
+        "title": "This is from context of about page",
+        "content": "About Page"
+    }
+    return render(request, "home_page.html", context)
+
+
+# make ContactForm for this
+def contact_page(request):
+    # contact form class instance
+    contact_form = ContactForm(request.POST or None)
+    context = {
+        "title": "This is from context of contact page",
+        "form": contact_form
+    }
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+
+    return render(request, "contact/contact.html", context)
+
+
+# make LoginForm for this
+def login_page(request):
+    # create instance of LoginForm
+    login_form = LoginForm(request.POST or None)
+    context = {
+        "form": login_form
+    }
+    print("user logged in")
+    print(request.user.is_authenticated())
+
+    if login_form.is_valid():
+        print(login_form.cleaned_data)
+        username = login_form.cleaned_data.get("username")
+        password = login_form.cleaned_data.get("password")
+        user = authenticate(request, username=username, password=password)
+        print(request.user.is_authenticated())
+        
+        if user is not None:
+            # login function for making the user login
+            login(request, user) # makes the user Login
+            context['form'] = LoginForm()
+            # redirect to a success page
+            return redirect("/")
+        else:
+            print("Error in authentication...")
+
+    return render(request, "auth/login.html", context)
+
+
+# make RegisterForm for this
+def register_page(request):
+    # create instance of RegisterForm
+    register_form = RegisterForm(request.POST or None)
+
+    context = {
+        "form": register_form
+    }
+    if register_form.is_valid():
+        print(register_form.cleaned_data)
+
+    return render(request, "auth/register.html", context)
