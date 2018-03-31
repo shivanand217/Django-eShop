1d1391b41a2a3dbebb58cc9ea4e0ee7cd59f9d71
Shiv Prakash Anand <shivakp2111@gmail.com>
2018-03-31 21:22:12 +0530

    31-03-2018

--- products/models.py
+++ products/models.py
@@ -1,21 +1,16 @@
 from django.db import models
 
 # All Django models must inherit from models.Model
-# everytime saving something in our models do,  [makemigrations and migrate]
-
+# Everytime saving something in our models do,  [makemigrations and migrate]
 
 class Product(models.Model):
-
+    
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(decimal_places= 2, max_digits= 10, default= 39.99)
-    image = models.FileField(upload_to='products/', null=True, blank=True)
+    image = models.FileField(upload_to='products/', null= True, blank= False)
 
     def __str__(self):
         return self.title
-
-    # on python2
-    '''
-    def __unicode__(self):
-        return self.title
-    '''
+    
+    
