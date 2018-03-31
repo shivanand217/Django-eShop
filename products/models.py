a5717304058970215588de54ded8a71c797d5ccf
Shiv Prakash Anand <shivakp2111@gmail.com>
2018-03-31 22:50:57 +0530

    31-03-2018

--- products/models.py
+++ products/models.py
@@ -11,21 +11,23 @@ def get_filename_ext(filepath):
     name, ext = os.path.splitext(base_name) # separates filename with its extension
     return name, ext
 
+#for giving a new random pathname
 def upload_image_path(instance, filename):
-    
+
     print(instance)
     print(filename)
     # changing filename
     new_filename = random.randint(1, 3910209312)
     name, ext = get_filename_ext(filename)
     final_filename = '{new_filename}{ext}'.format(new_filename= new_filename, ext= ext)
-    return "products/{new_filename}/{final_filename}".format(new_filename=new_filename,final_filename=final_filename)
+    return "products/{new_filename}/{final_filename}".format(new_filename= new_filename,final_filename= final_filename)
 
 class Product(models.Model):
+
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(decimal_places= 2, max_digits= 10, default= 39.99)
-    image = models.FileField(upload_to='products/', null= True, blank= True)
+    image = models.FileField(upload_to= upload_image_path , null= True, blank= True)
 
     def __str__(self):
         return self.title
