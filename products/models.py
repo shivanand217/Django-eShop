5513826f7ce733495ba3510ff8b8c243cc37d7c4
Shiv Prakash Anand <shivakp2111@gmail.com>
2018-04-01 00:54:34 +0530

    done some migrations

--- products/models.py
+++ products/models.py
@@ -21,11 +21,18 @@ def upload_image_path(instance, filename):
     final_filename = '{new_filename}{ext}'.format(new_filename= new_filename, ext= ext)
     return "products/{new_filename}/{final_filename}".format(new_filename= new_filename,final_filename= final_filename)
 
+
 class Product(models.Model):
+    
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(decimal_places= 2, max_digits= 10, default= 39.99)
     image = models.ImageField(upload_to= upload_image_path , null= True, blank= True)
+    featured = models.BooleanField(default= False)
+    active = models.BooleanField(default= True)
+    timestamp = models.DateTimeField(auto_now_add= True)
+    is_digital = models.BooleanField(default= False)
+
 
     def __str__(self):
         return self.title
