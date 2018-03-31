b078e5b0451be6720c263ee3e6a36c1df4782665
Shiv Prakash Anand <shivakp2111@gmail.com>
2018-03-31 23:28:22 +0530

    31-03-2018

--- products/models.py
+++ products/models.py
@@ -7,7 +7,6 @@ import os
 # Everytime saving something in our models do,  [makemigrations and migrate]
 
 def get_filename_ext(filepath):
-    
     base_name = os.path.basename(filepath) # returns basename of the file
     name, ext = os.path.splitext(base_name) # separates filename with its extension
     return name, ext
@@ -24,15 +23,14 @@ def upload_image_path(instance, filename):
     return "products/{new_filename}/{final_filename}".format(new_filename= new_filename,final_filename= final_filename)
 
 class Product(models.Model):
-
+    
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(decimal_places= 2, max_digits= 10, default= 39.99)
-    image = models.FileField(upload_to= upload_image_path , null= True, blank= True)
+    image = models.ImageField(upload_to= upload_image_path , null= True, blank= True)
 
     def __str__(self):
         return self.title
 
     def __unicode__(self):
         return self.title
-    
