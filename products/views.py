6e7496fc7eeabb92d3fa65a76278e98f71419425
Shiv Prakash Anand <shivakp2111@gmail.com>
2018-04-02 00:40:39 +0530

    02-04-2018

--- products/views.py
+++ products/views.py
@@ -7,9 +7,7 @@ from django.http import Http404
 # import all the views to urls to make action
 # both function-based and class-based views are independent their urls are different
 
-
 class ProductListView(ListView):
-    queryset = Product.objects.all()
     template_name = "products/list.html"
     
     def get_context_data(self, *args, **kwargs):
@@ -17,24 +15,31 @@ class ProductListView(ListView):
         print(context)
         return context
 
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return Product.objects.all()
+
+
 def product_list_view(request):
     queryset = Product.objects.all()
-
     context = {
         "object_list": queryset
     }
     return render(request, "products/list.html", context)
 
 class ProductDetailView(DetailView):
+
     queryset = Product.objects.all()
-    template_name = "products/cb-detail.html"
+    template_name = "products/fb-detail.html"
 
     def get_object(self, *args, **kwargs):
         request = self.request
         pk = self.kwargs.get('pk')
-        instance = Product.objects.get_by_id(pk)
-        if instance is None:
-            raise Http404("Product does nnot exists.")
+        qs = Product.objects.get_by_id(pk)
+        if qs.exists() and qs.count() == 1:
+            instance = qs.first()
+        else:
+            raise Http404("Product doesn't exists.")
         return instance
 
     def get_context_data(self, *args, **kwargs):
@@ -55,8 +60,8 @@ def product_detail_view(request, pk=None, *args, **kwargs):
             print("no product here")
             raise Http404("product does not exists")
     '''
-
     qs = Product.objects.get_by_id(pk)
+
     if qs.exists() and qs.count() == 1:
         instance = qs.first()
     else:
@@ -66,5 +71,4 @@ def product_detail_view(request, pk=None, *args, **kwargs):
         "object": instance,
         "pkk": pk
     }
-
     return render(request, "products/fb-detail.html", context)
