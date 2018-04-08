4aa48f69991d30d3ab0b02c8d5ab2f271f3239e5
shivanand217 <apple@apples-MacBook-Air.local>
2018-04-08 17:37:38 +0530

    slight change

--- products/models.py
+++ products/models.py
@@ -11,7 +11,6 @@ def get_filename_ext(filepath):
     name, ext = os.path.splitext(base_name) # separates filename with its extension
     return name, ext
 
-
 #for giving a new random pathname
 def upload_image_path(instance, filename): 
     print(instance)
@@ -22,13 +21,11 @@ def upload_image_path(instance, filename):
     final_filename = '{new_filename}{ext}'.format(new_filename= new_filename, ext= ext)
     return "products/{new_filename}/{final_filename}".format(new_filename= new_filename,final_filename= final_filename)
 
-
 class ProductManager(models.Manager):
     def get_by_id(self, id):
         return self.get_queryset().filter(id=id) # Product.objects === self.get_queryset()
 
 class Product(models.Model):
-
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(decimal_places= 2, max_digits= 10, default= 39.99)
